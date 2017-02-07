526147f9159ab649285cad788f542ff20d51ea41
Natasha Dhital <natashadhital@Natashas-Air.local.tld>
2017-02-07 15:52:58 -0800

    first push

---
+++ checker.c
@@ -0,0 +1,58 @@
+#include <checker.h>
+
+int     ft_atoi(char *str)
+{
+    int value;
+    int negative;
+
+    value = 0;
+    negative = (*str == '-') ? -1 : 1;
+    if (*str == '-')
+        str++;
+    while (*str != 0)
+        value = value * 10 + (*str++ - '0');
+    return (value * negative);
+}
+
+int     valid_int(char *str)
+{
+    if (*str == '-')
+        str++;
+    while (*str != 0)
+    {
+        if (*str < '0' || *str > '9')
+            return (0);
+        str++;
+    }
+    return (1);
+}
+
+int     main(int argc, char **argv)
+{
+    int     i;
+    int     *iargs;
+    char    *str;
+
+    i = 0;
+    if (argc < 2)
+        ft_putstr("Error\n");
+    else
+    {
+        iargs = (int *)malloc(sizeof(int) * argc);
+        while (argc > 1)
+        {
+            if (!valid_int(argv[--argc]))
+            {
+                ft_putstr("Error\n");
+                return (0);
+            }
+            iargs[i++] = ft_atoi(argv[argc]);
+        }
+        iargs[i] = '\0';
+    }
+    while(get_next_line(0, &str))
+    {
+        
+    }
+    free(iargs);
+}
---
+++ checker.h
@@ -0,0 +1,18 @@
+#ifndef CHECKER_H
+# define CHECKER_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include "./get_next_line/get_next_line.h"
+
+typedef struct  t_stack
+{
+    int     *a;
+    int     *b;
+    size_t  asize;
+    size_t  bsize;
+}               f_stack;
+
+
+
+#endif
